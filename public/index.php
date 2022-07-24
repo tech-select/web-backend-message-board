013ee7bf6027782f4cdf404a23bc53bfa0a50561
yuyaamano23 <nba.chako.nba@icloud.com>
2022-07-24 19:12:29 +0900

    add: セッションを利用して値を保持する

--- public/index.php
+++ public/index.php
@@ -1,13 +1,14 @@
 <?php
+session_start();
 require_once(__DIR__ . '/../src/db_connect.php');
 
 if (isset($_POST['action_type']) && $_POST['action_type']) {
   if ($_POST['action_type'] === 'insert') {
     require(__DIR__ . '/../src/insert_message.php');
-  } elseif ($_POST['action_type'] === 'delete') {
-    require(__DIR__ . '/../src/delete_message.php');
   }
 }
+
+require(__DIR__ . '/../src/session_values.php');
 
 function convertTz($datetime_text)
 {
@@ -35,13 +36,19 @@ function convertTz($datetime_text)
     <div class="form-cover">
       <form action="/" method="post">
         <div class="form-input-title">投稿者ニックネーム</div>
-        <input type="text" name="author_name" maxlength="40" value="" class="input-author-name" />
-        <div class="form-input-error">
-        </div>
+        <input type="text" name="author_name" maxlength="40" value="<?php echo htmlspecialchars($messages['input_pre_author_name'], ENT_QUOTES); ?>" class="input-author-name" />
+        <?php if ($messages['input_error_author_name'] !== '') { ?>
+          <div class="form-input-error">
+            <?= $messages['input_error_author_name']; ?>
+          </div>
+        <?php } ?>
         <div class="form-input-title">投稿内容<small>(必須)</small></div>
-        <textarea name="message" class="input-message"></textarea>
-        <div class="form-input-error">
-        </div>
+        <textarea name="message" class="input-message"><?php echo htmlspecialchars($messages['input_pre_message'], ENT_QUOTES); ?></textarea>
+        <?php if ($messages['input_error_message'] !== '') { ?>
+          <div class="form-input-error">
+            <?= $messages['input_error_message']; ?>
+          </div>
+        <?php } ?>
         <input type="hidden" name="action_type" value="insert" />
         <button type="submit" class="input-submit-button">投稿する</button>
       </form>
--- src/insert_message.php
+++ src/insert_message.php
@@ -14,12 +14,14 @@ $is_valid_auther_name = true;
 $input_author_name = '';
 if (isset($_POST['author_name'])) {
     $input_author_name = mbTrim(str_replace("\r\n", "\n", $_POST['author_name']));
+    $_SESSION['input_pre_author_name'] = $_POST['author_name'];
 } else {
     $is_valid_auther_name = false;
 }
 
 if ($is_valid_auther_name && mb_strlen($input_author_name) > 30) {
     $is_valid_auther_name = false;
+    $_SESSION['input_error_author_name'] = 'ニックネームは 30 文字以内で入力してください。（現在 ' . mb_strlen($input_author_name) . ' 文字）';
 }
 
 // 入力値を確認する（投稿内容）
@@ -27,16 +29,19 @@ $is_valid_message = true;
 $input_message = '';
 if (isset($_POST['message'])) {
     $input_message = mbTrim(str_replace("\r\n", "\n", $_POST['message']));
+    $_SESSION['input_pre_message'] = $_POST['message'];
 } else {
     $is_valid_message = false;
 }
 
 if ($is_valid_message && $input_message === '') {
     $is_valid_message = false;
+    $_SESSION['input_error_message'] = '投稿内容の入力は必須です。';
 }
 
 if ($is_valid_message && mb_strlen($input_message) > 1000) {
     $is_valid_message = false;
+    $_SESSION['input_error_message'] = '投稿内容は 1000 文字以下で入力してください。（現在 ' . mb_strlen($input_message) . ' 文字）';
 }
 
 // 投稿をデータベースへ保存する処理
@@ -58,6 +63,10 @@ if ($is_valid_auther_name && $is_valid_message) {
 
     // クエリを実行する
     $stmt->execute();
+    $_SESSION['input_error_author_name'] = '';
+    $_SESSION['input_error_message'] = '';
+    $_SESSION['input_pre_author_name'] = '';
+    $_SESSION['input_pre_message'] = '';
 }
 
 header('Location: /');
